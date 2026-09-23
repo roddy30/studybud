4e63f33ae96fc80ec8ee047d83bbbbdb0e381ad9
Rod Lebumfacil <rodlebumfacilio@gmail.com>
2026-09-23 18:14:36 +0800

    Update Firebase project to studybud-quiz

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -4,20 +4,20 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 const firebaseConfig = {
   apiKey:
     process.env.NEXT_PUBLIC_FIREBASE_API_KEY ||
-    'AIzaSyBC3ws9Uppu_jxy1l-Mxoov7Ruvf_cVIGI',
+    'AIzaSyBj2dNVQ_sYB3AJD6y6z_-o9cPTaXp3XJo',
   authDomain:
     process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ||
-    'studbud-39542.firebaseapp.com',
+    'studybud-quiz.firebaseapp.com',
   projectId:
-    process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || 'studbud-39542',
+    process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || 'studybud-quiz',
   storageBucket:
     process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ||
-    'studbud-39542.firebasestorage.app',
+    'studybud-quiz.firebasestorage.app',
   messagingSenderId:
-    process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '800835639356',
+    process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '139552647625',
   appId:
     process.env.NEXT_PUBLIC_FIREBASE_APP_ID ||
-    '1:800835639356:web:3574742921fe4e89a70b39',
+    '1:139552647625:web:18881dd434561fab00a018',
 };
 
 // Initialize Firebase once
@@ -26,7 +26,7 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 
-// Optional: Force account picker prompt
+// Optional: Prompt account chooser
 googleProvider.setCustomParameters({
   prompt: 'select_account',
 });
